3173c5e8aa52a7d2bb4f80ece41f3e44352f0622
caesarnay <caesar@test.com>
2020-03-24 17:13:03 +0630

    test

--- app.js
+++ app.js
@@ -66,6 +66,8 @@ app.post('/webhook', (req, res) => {
 
 });
 
+app.use('/uploads', express.static('uploads'))
+
 //webview test
 app.get('/webview/:sender_id',function(req,res){
     const sender_id = req.params.sender_id;
