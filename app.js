ce4df6a2a1f7e1a13f4bd931c148b02ca8ebdf3d
caesarnay <caesar@test.com>
2020-03-25 16:15:50 +0630

    Test

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var storage = multer.diskStorage({
     cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname + '-' + Date.now())
+    cb(null, file.originalname);
   }
 })
 
