7b4ce418a2635fbc88e4984ccf09a603abbbdc9e
raseacyan <caesar821009@gmail.com>
2020-04-10 12:49:11 +0630

    test

--- app.js
+++ app.js
@@ -410,7 +410,7 @@ const handleMessage = (sender_psid, received_message) => {
         case "add book":
           addBooks(sender_psid);
           break;
-        case "add review"
+        case "add review":
           addReview(sender_psid);
           break;
         default:
