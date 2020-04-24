4a4e5e8d07aba97c488169db060b8053c576073a
raseacyan <caesar821009@gmail.com>
2020-04-24 12:49:32 +0630

    test

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ app.get('/updateprivatetour/:booking_number/:sender_id/',function(req,res){
 
             console.log("BOOKING DATA", data);     
 
-            res.render('updateprivetetour.ejs',{data:data, sender_id:sender_id});
+            res.render('updateprivatetour.ejs',{data:data, sender_id:sender_id});
             
 
         });
