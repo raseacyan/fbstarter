e7cedb6705c0c946f70e37698702bac09d68c734
raseacyan <caesar821009@gmail.com>
2020-08-26 13:59:56 +0630

    test

--- app.js
+++ app.js
@@ -29,7 +29,13 @@ var storage = multer.diskStorage({
   }
 })*/
 
-//const upload = multer({ storage: storage });
+const upload = multer({ 
+  storage: multer.memoryStorage(),
+  limits :{
+    fileSize: 5 * 1024 * 1024  //no larger than 5mb
+  }
+
+});
 
 // parse application/x-www-form-urlencoded
 
