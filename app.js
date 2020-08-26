988f3282ae62976a76f5c1a4f256158eae93a5a2
raseacyan <caesar821009@gmail.com>
2020-08-26 18:58:23 +0630

    test

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var storage = multer.diskStorage({
 const upload = multer({ 
   storage: multer.memoryStorage(),
   limits :{
-    fileSize: 5 * 1024 * 1024  //no larger than 5mb
+    fileSize: 50 * 1024 * 1024  //no larger than 5mb
   }
 
 });
