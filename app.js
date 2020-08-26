87c258dcec58d53b88cf3f4f0c4c403c015ca682
raseacyan <caesar821009@gmail.com>
2020-08-26 18:46:25 +0630

    test

--- app.js
+++ app.js
@@ -222,27 +222,16 @@ app.post('/webview',upload.single('file'),function(req,res){
 
 
 
-      const gcsname = Date.now() + req.file.originalname;
-      const file = bucket.file(gcsname);
-
-      const stream = file.save({
-        metadata: {
-          contentType: req.file.mimetype
-        },
-        resumable: false
-      });
-
-      stream.on('error', (err) => {
-        req.file.cloudStorageError = err;
-        console.log('error');
-      });
-
-      stream.on('finish', () => {
-        req.file.cloudStorageObject = gcsname;
-        console.log('finish');
-      });
-
-      stream.end(req.file.buffer);
+      let file = req.file;
+      if (file) {
+        uploadImageToStorage(file).then((success) => {
+          res.status(200).send({
+            status: 'success'
+          });
+        }).catch((error) => {
+          console.error(error);
+        });
+      }
 
 
 
@@ -681,6 +670,36 @@ const callSendAPI = (sender_psid, response) => {
 async function callSend(sender_psid, response){
   let send = await callSendAPI(sender_psid, response);
   return 1;
+}
+
+
+const uploadImageToStorage = (file) => {
+  return new Promise((resolve, reject) => {
+    if (!file) {
+      reject('No image file');
+    }
+    let newFileName = `${file.originalname}_${Date.now()}`;
+
+    let fileUpload = bucket.file(newFileName);
+
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mimetype
+      }
+    });
+
+    blobStream.on('error', (error) => {
+      reject('Something is wrong! Unable to upload at the moment.');
+    });
+
+    blobStream.on('finish', () => {
+      // The public URL can be used to directly access the file via HTTP.
+      const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
+      resolve(url);
+    });
+
+    blobStream.end(file.buffer);
+  });
 }
 
 
